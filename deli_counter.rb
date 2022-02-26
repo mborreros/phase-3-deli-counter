e024108a6c29ec49c374200b5aedddab164f478d
mborreros <mlbsindia@gmail.com>
2022-02-26 14:24:15 -0500

    commit tree 6203b1ad0d5b912711a5ef5572653ef9aa6815f1

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def line(line)
+  if line.empty?
+    puts "The line is currently empty."
+  else
+    text = ["The line is currently:"]
+    for index in (0...line.length)
+      numb_text = "#{index+1}. #{line[index]}"
+      text << numb_text
+    end
+    puts text.join(" ")
+  end
+end
+
+def take_a_number(line, name)
+  line << name
+  puts "Welcome, #{name}. You are number #{line.length} in line."
+end
+
+def now_serving(line)
+  if line.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{line.shift}."
+  end
+end
+
